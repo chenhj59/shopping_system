72e024a6da4e0bc8c0d88a5100aca8c787ca7be2
陈 豪嘉 <chenhj@mail.ynu.edu.cn>
2023-08-12 09:59:49 +0000

    V1.2

--- src/main/java/org/example/Admin/AdminDatebaseInitializer.java
+++ src/main/java/org/example/Admin/AdminDatebaseInitializer.java
@@ -1,55 +1,71 @@
 package org.example.Admin;
 import org.example.DatebaseInitializer;
 
-import java.sql.*;
-
 import java.util.ArrayList;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.*;
 import java.io.*;
 public class AdminDatebaseInitializer implements DatebaseInitializer{
 
+    private static final String fileName = "adminAccounts.xlsx";
     @Override
     public void initializeDatabase() {
-        File file = new File("adminAccounts.txt");
+        File file = new File(fileName);
         
         try {
             if (!file.exists()) {
-                boolean created = file.createNewFile();
-                if (created) {
-                    System.out.println("文件已创建: " + file.getName());
-                } else {
-                    System.out.println("无法创建文件。");
-                }
+                XSSFWorkbook workbook = new XSSFWorkbook();
+                XSSFSheet sheet = workbook.createSheet("AdminAccounts");
+                FileOutputStream outputStream = new FileOutputStream(file);
+                workbook.write(outputStream);
+                outputStream.close();
+                System.out.println("Excel 文件已创建: " + file.getName());
             } else {
-                System.out.println("文件已存在: " + file.getName());
+                System.out.println("Excel 文件已存在: " + file.getName());
             }
         } catch (IOException e) {
             System.out.println("发生IO异常: " + e.getMessage());
         }
     }
 
-public void read(ArrayList<Admin> admins) {
-    try (BufferedReader br = new BufferedReader(new FileReader("adminAccounts.txt"))) {
-        String line;
-        while ((line = br.readLine()) != null) {
-            String[] info = line.split(",");
-            admins.add(new Admin(info[0], info[1]));
+    public void read(ArrayList<Admin> admins) {
+        try (FileInputStream file = new FileInputStream(fileName)) {
+            Workbook workbook = new XSSFWorkbook(file);
+            Sheet sheet = workbook.getSheetAt(0);
+            for (Row row : sheet) {
+                String name = row.getCell(0).getStringCellValue();
+                String password = row.getCell(1).getStringCellValue();
+                admins.add(new Admin(name, password));
+            }
+            workbook.close();
+        } catch (IOException e) {
+            System.out.println("读取文件失败: " + e.getMessage());
         }
-    } catch (IOException e) {
-        System.out.println("读取文件失败: " + e.getMessage());
     }
-}
 
-public void write(ArrayList<Admin> admins) {
-    try (BufferedWriter bw = new BufferedWriter(new FileWriter("adminAccounts.txt"))) {
-        for (Admin admin : admins) {
-            String line = admin.getName() + "," + admin.getPassword();
-            bw.write(line);
-            bw.newLine();
+    public void write(ArrayList<Admin> admins) {
+        try (FileInputStream file = new FileInputStream(fileName);
+            Workbook workbook = new XSSFWorkbook(file)) {
+            Sheet sheet = workbook.getSheetAt(0);
+            int rowCount = 0;
+            for (Admin admin : admins) {
+                Row row = sheet.getRow(rowCount);
+                if (row == null) {
+                    row = sheet.createRow(rowCount);
+                }
+                Cell nameCell = row.createCell(0);
+                nameCell.setCellValue(admin.getName());
+                Cell passwordCell = row.createCell(1);
+                passwordCell.setCellValue(admin.getPassword());
+                rowCount++;
+            }
+
+            FileOutputStream outputStream = new FileOutputStream(fileName);
+            workbook.write(outputStream);
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    } catch (IOException e) {
-        e.printStackTrace();
     }
-}
-
 }
--- src/main/java/org/example/User/UserDatebaseInitializer.java
+++ src/main/java/org/example/User/UserDatebaseInitializer.java
@@ -1,58 +1,30 @@
 package org.example.User;
-import java.sql.*;
 import java.util.ArrayList;
 
 import org.example.DatebaseInitializer;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.*;
+
 import java.io.*;
 public class UserDatebaseInitializer implements DatebaseInitializer{
+    private static final String fileName = "userData.xlsx";
     @Override
     public void initializeDatabase() {
-        File file = new File("usersAccount.txt");
-        
-        try {
-            if (!file.exists()) {
-                boolean created = file.createNewFile();
-                if (created) {
-                    System.out.println("文件已创建: " + file.getName());
-                } else {
-                    System.out.println("无法创建文件。");
-                }
-            } else {
-                System.out.println("文件已存在: " + file.getName());
-            }
-        } catch (IOException e) {
-            System.out.println("发生IO异常: " + e.getMessage());
-        }
-        file = new File("usersShoppingCart.txt");
-        
-        try {
-            if (!file.exists()) {
-                boolean created = file.createNewFile();
-                if (created) {
-                    System.out.println("文件已创建: " + file.getName());
-                } else {
-                    System.out.println("无法创建文件。");
-                }
-            } else {
-                System.out.println("文件已存在: " + file.getName());
-            }
-        } catch (IOException e) {
-            System.out.println("发生IO异常: " + e.getMessage());
-        }
+        File file = new File(fileName);
 
-        file = new File("usersPurchaseRecord.txt");
-        
         try {
             if (!file.exists()) {
-                boolean created = file.createNewFile();
-                if (created) {
-                    System.out.println("文件已创建: " + file.getName());
-                } else {
-                    System.out.println("无法创建文件。");
-                }
+                XSSFWorkbook workbook = new XSSFWorkbook();
+                XSSFSheet userSheet = workbook.createSheet("usersAccount");
+                XSSFSheet cartSheet = workbook.createSheet("usersShoppingCart");
+                XSSFSheet purchaseSheet = workbook.createSheet("usersPurchaseRecord");
+                FileOutputStream outputStream = new FileOutputStream(file);
+                workbook.write(outputStream);
+                outputStream.close();
+                System.out.println("Excel 文件已创建: " + file.getName());
             } else {
-                System.out.println("文件已存在: " + file.getName());
+                System.out.println("Excel 文件已存在: " + file.getName());
             }
         } catch (IOException e) {
             System.out.println("发生IO异常: " + e.getMessage());
@@ -60,68 +32,108 @@ public class UserDatebaseInitializer implements DatebaseInitializer{
     }
 
     public void read(ArrayList<User> users, ArrayList<CartItem> cartItems, ArrayList<PurchaseItem> purchaseItems) {
-        try (BufferedReader br = new BufferedReader(new FileReader("usersAccount.txt"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] info = line.split(",");
-                users.add(new User(Integer.parseInt(info[0]), info[1], info[2], info[3], info[4], Double.parseDouble(info[5]), info[6], info[7]));
+        try (FileInputStream file = new FileInputStream(fileName)) {
+            Workbook workbook = new XSSFWorkbook(file);
+            // 读取用户账户数据
+            Sheet userSheet = workbook.getSheetAt(0);
+            for (Row row : userSheet) {
+                int id = (int) row.getCell(0).getNumericCellValue();
+                String username = row.getCell(1).getStringCellValue();
+                String password = row.getCell(2).getStringCellValue();
+                String userLevel = row.getCell(3).getStringCellValue();
+                String registerTime = row.getCell(4).getStringCellValue();
+                double totalCost = row.getCell(5).getNumericCellValue();
+                String phoneNumber = row.getCell(6).getStringCellValue();
+                String email = row.getCell(7).getStringCellValue();
+                users.add(new User(id, username, password, userLevel, registerTime, totalCost, phoneNumber, email));
             }
-        } catch (IOException e) {
-            System.out.println("读取文件失败: " + e.getMessage());
-        }
 
-        try (BufferedReader br = new BufferedReader(new FileReader("usersShoppingCart.txt"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] info = line.split(",");
-                cartItems.add(new CartItem(Integer.parseInt(info[0]), Integer.parseInt(info[1]), info[2], Double.parseDouble(info[3]), Integer.parseInt(info[4])));
+            // 读取购物车数据
+            Sheet cartSheet = workbook.getSheetAt(1);
+            for (Row row : cartSheet) {
+                int id = (int) row.getCell(0).getNumericCellValue();
+                int userID = (int) row.getCell(1).getNumericCellValue();
+                String name = row.getCell(2).getStringCellValue();
+                double purcPrice = row.getCell(3).getNumericCellValue();
+                int quantity = (int) row.getCell(4).getNumericCellValue();
+                cartItems.add(new CartItem(id, userID, name, purcPrice, quantity));
             }
-        } catch (IOException e) {
-            System.out.println("读取文件失败: " + e.getMessage());
-        }
 
-        try (BufferedReader br = new BufferedReader(new FileReader("usersPurchaseRecord.txt"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] info = line.split(",");
-                purchaseItems.add(new PurchaseItem(Integer.parseInt(info[0]), Integer.parseInt(info[1]), info[2], Double.parseDouble(info[3]), Integer.parseInt(info[4]), info[5]));
+            // 读取购买记录数据
+            Sheet purchaseSheet = workbook.getSheetAt(2);
+            for (Row row : purchaseSheet) {
+                int id = (int) row.getCell(0).getNumericCellValue();
+                int userID = (int) row.getCell(1).getNumericCellValue();
+                String name = row.getCell(2).getStringCellValue();
+                double purcPrice = row.getCell(3).getNumericCellValue();
+                int quantity = (int) row.getCell(4).getNumericCellValue();
+                String time = row.getCell(5).getStringCellValue();
+                purchaseItems.add(new PurchaseItem(id, userID, name, purcPrice, quantity, time));
             }
+            workbook.close();
         } catch (IOException e) {
-            System.out.println("读取文件失败: " + e.getMessage());
+            e.printStackTrace();
         }
     }
-
-    public void write(ArrayList<User> users, ArrayList<CartItem> cartItems, ArrayList<PurchaseItem> purchaseItems) {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter("usersAccount.txt"))) {
+    public void write(ArrayList<User> users, ArrayList<CartItem> cartItems, ArrayList<PurchaseItem> purchaseItems){
+        try (FileInputStream file = new FileInputStream(fileName);
+            Workbook workbook = new XSSFWorkbook(file)) {
+            Sheet userSheet = workbook.getSheetAt(0);
+            
+            int userRowNum = 0;
             for (User user : users) {
-                String line = user.getID() + "," + user.getUsername() + "," + user.getPassword() + "," + user.getUserLevel() + "," + user.getRegisterTime() +
-                        "," + user.getTotalCost() + "," + user.getPhoneNumber() + "," + user.getEmail();
-                bw.write(line);
-                bw.newLine();
+                Row row = userSheet.getRow(userRowNum);
+                if (row == null) {
+                    row = userSheet.createRow(userRowNum);
+                }
+                row.createCell(0).setCellValue(user.getID());
+                row.createCell(1).setCellValue(user.getUsername());
+                row.createCell(2).setCellValue(user.getPassword());
+                row.createCell(3).setCellValue(user.getUserLevel());
+                row.createCell(4).setCellValue(user.getRegisterTime());
+                row.createCell(5).setCellValue(user.getTotalCost());
+                row.createCell(6).setCellValue(user.getPhoneNumber());
+                row.createCell(7).setCellValue(user.getEmail());
+                userRowNum++;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter("usersShoppingCart.txt"))) {
+            
+            Sheet cartSheet = workbook.getSheetAt(1);
+            int cartRowNum = 0;
             for (CartItem cartItem : cartItems) {
-                String line = cartItem.getID() + "," + cartItem.getUserID() + "," + cartItem.getName() + "," + cartItem.getPurcPrice() + "," + cartItem.getQuantity();
-                bw.write(line);
-                bw.newLine();
+                Row row = cartSheet.getRow(cartRowNum);
+                if (row == null) {
+                    row = cartSheet.createRow(cartRowNum);
+                }
+                row.createCell(0).setCellValue(cartItem.getID());
+                row.createCell(1).setCellValue(cartItem.getUserID());
+                row.createCell(2).setCellValue(cartItem.getName());
+                row.createCell(3).setCellValue(cartItem.getPurcPrice());
+                row.createCell(4).setCellValue(cartItem.getQuantity());
+                cartRowNum++;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter("usersPurchaseRecord.txt"))) {
+            Sheet purchaseSheet = workbook.getSheetAt(2);
+            int purchaseRowNum = 0;
             for (PurchaseItem purchaseItem : purchaseItems) {
-                String line = purchaseItem.getID() + "," + purchaseItem.getUserID() + "," + purchaseItem.getName() + "," + purchaseItem.getPurcPrice() + ","
-                        + purchaseItem.getQuantity() + "," + purchaseItem.getTime();
-                bw.write(line);
-                bw.newLine();
+                Row row = purchaseSheet.getRow(purchaseRowNum);
+                if (row == null) {
+                    row = purchaseSheet.createRow(purchaseRowNum);
+                }
+                row.createCell(0).setCellValue(purchaseItem.getID());
+                row.createCell(1).setCellValue(purchaseItem.getUserID());
+                row.createCell(2).setCellValue(purchaseItem.getName());
+                row.createCell(3).setCellValue(purchaseItem.getPurcPrice());
+                row.createCell(4).setCellValue(purchaseItem.getQuantity());
+                row.createCell(5).setCellValue(purchaseItem.getTime());
+                purchaseRowNum++;
             }
+            
+            FileOutputStream outputStream = new FileOutputStream(fileName);
+            workbook.write(outputStream);
+            outputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
 }
